ce52d99905389ed31dbc5ead33c4f84ce99ec435
aechon <tony.hronowski@gmail.com>
2025-01-09 15:48:37 -0500

    fix another syntax error

--- backend/db/models/detour.js
+++ backend/db/models/detour.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Detour.belongsTo(models.Trips, {foreignKey: 'trip_id'});
+      Detour.belongsTo(models.Trip, {foreignKey: 'trip_id'});
     }
   }
   Detour.init({
